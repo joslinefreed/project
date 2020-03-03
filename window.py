1a3a58806983f193d720c6a11005c4a8b70cfe45
joslinefreed <48320250+joslinefreed@users.noreply.github.com>
2020-03-03 12:30:42 +0000

    Add files via upload

--- window.py
+++ window.py
@@ -31,10 +31,12 @@ class NumericTableWidgetItem(QTableWidgetItem):
 class FirstWindow(QtWidgets.QMainWindow, win1):
 
     # Set all initial selected values to None
-    selectedStockHeader = None
-    selectedCustomerHeader = None
-    selectedHeaderHeader = None
+    selectedHeader = [None]*3
     selectedOrderNumber = None
+    selectedCustomerName = None
+    currentTab = 0
+
+    loadedTab = [False]*3
 
     # define database so it can be used throughout the class
     db = 'Book Selling Database.db'
@@ -43,39 +45,47 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         QtWidgets.QMainWindow.__init__(self, parent)
         # load all the data into the tables
         self.setupUi(self)
-        self.refreshStock()
+        self.refreshTab(0)
 
         # hide help text
         self.helpWidget.hide()
 
-        # Get the header view from the customer table and connect it for when the section is clicked
-        customer_table: QTableWidget = self.findCustomerTable()
-        customer_header: QHeaderView = customer_table.horizontalHeader()
-        customer_header.sectionClicked.connect(self.customerHeaderSectionClicked)
-
         # Get the header view from the stock table and connect it for when the section is clicked
         stock_table: QTableWidget = self.findStockTable()
         stock_header: QHeaderView = stock_table.horizontalHeader()
-        stock_header.sectionClicked.connect(self.stockHeaderSectionClicked)
+        stock_header.sectionClicked.connect(self.headerSectionClicked)
+
+        # Get the header view from the customer table and connect it for when the section is clicked
+        customer_table: QTableWidget = self.findCustomerTable()
+        customer_header: QHeaderView = customer_table.horizontalHeader()
+        customer_header.sectionClicked.connect(self.headerSectionClicked)
 
         # Get the header view from the stock table and connect it for when the section is clicked
         headers_table: QTableWidget = self.findHeadersTable()
         headers_horizontal_header: QHeaderView = headers_table.horizontalHeader()
-        headers_horizontal_header.sectionClicked.connect(self.headerHorizontalHeaderSectionClicked)
+        headers_horizontal_header.sectionClicked.connect(self.headerSectionClicked)
         headers_vertical_header: QHeaderView = headers_table.verticalHeader()
         headers_vertical_header.sectionClicked.connect(self.headerVerticalHeaderSectionClicked)
 
     def refreshTab(self, tabnumber: int):
-        if tabnumber == 0:
-            self.refreshStock()
-        elif tabnumber == 1:
-            self.refreshCustomers()
-        elif tabnumber == 2:
-            self.refreshHeader()
-        else:
-            pass
 
-    def refreshColumn(self, results, column, table, size, numeric=False):
+        # check tab has not been loaded
+        if not self.loadedTab[tabnumber]:
+            # load data for the relevant table
+            if tabnumber == 0:
+                self.refreshStock()
+            elif tabnumber == 1:
+                self.refreshCustomers()
+            elif tabnumber == 2:
+                self.refreshHeader()
+            # set that tab to loaded
+            self.loadedTab[tabnumber] = True
+            # update current tab number
+            self.currentTab = tabnumber
+        else:
+            self.currentTab = tabnumber
+
+    def refreshColumn(self, results, column, table, size, numeric=False, price=False):
 
         # Set the table to column to the size passed into the function
         table.setColumnWidth(column, size)
@@ -87,18 +97,66 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         # Set the starting index to 0
         row_index: int = 0
 
-        for row in results:
-            # Set the item in the table to being the name
-            name: str = [str(x) for x in row][0]
+        if not price:
+            for row in results:
+                # Set the item in the table to being the name
+                name: str = [str(x) for x in row][0]
 
-            if numeric:
-                # use our own class so that sorting of numeric values works correctly
-                table.setItem(row_index, column, NumericTableWidgetItem(name))
-            else:
-                table.setItem(row_index, column, QTableWidgetItem(name))
+                if numeric:
+                    # use our own class so that sorting of numeric values works correctly
+                    table.setItem(row_index, column, NumericTableWidgetItem(name))
+                else:
+                    table.setItem(row_index, column, QTableWidgetItem(name))
 
-            # Increment the row pointer
-            row_index = row_index + 1
+                # Increment the row pointer
+                row_index = row_index + 1
+        else:
+            for row in results:
+                # Set the item in the table to being the name
+                number = [self.getPrice(x) for x in row][0]
+
+                table.setItem(row_index, column, NumericTableWidgetItem(number))
+                row_index = row_index + 1
+
+    def getPrice(self, number):
+        price = float("{0:.2f}".format(number))
+        price = str(price)
+        return price
+
+    def headerSectionClicked(self, logicalindex: int):
+
+        # Find the relevant table and selected header
+        if self.currentTab == 0:
+            table: QTableWidget = self.findStockTable()
+            selectedHeader = self.selectedHeader[0]
+        elif self.currentTab == 1:
+            table: QTableWidget = self.findCustomerTable()
+            selectedHeader = self.selectedHeader[1]
+        elif self.currentTab == 2:
+            table: QTableWidget = self.findHeadersTable()
+            selectedHeader = self.selectedHeader[2]
+
+        header: QHeaderView = table.horizontalHeader()
+
+        # Check if the table is currently sorted in ascending order and if so sort in descending order
+        if selectedHeader != logicalindex:
+            table.sortItems(logicalindex, QtCore.Qt.AscendingOrder)
+            # Change indicator on the header
+            header.setSortIndicator(logicalindex, QtCore.Qt.AscendingOrder)
+            # set selectedHeader to the new selected header index
+            selectedHeader = logicalindex
+        else:
+            table.sortItems(logicalindex, QtCore.Qt.DescendingOrder)
+            # Change indicator on the header
+            header.setSortIndicator(logicalindex, QtCore.Qt.DescendingOrder)
+            # reset selectedHeader as the column is currently sorted in descending order
+            selectedHeader = None
+
+        # show indicator so clear it is clear to the user how it is sorted
+        header.setSortIndicatorShown(True)
+
+        # return the update selected header
+        self.selectedHeader[self.currentTab] = selectedHeader
 
     def findStockTable(self) -> QTableWidget:
 
@@ -113,6 +171,7 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
 
         # Find the stock table widget
         table: QTableWidget = self.findStockTable()
+        header: QHeaderView = table.horizontalHeader()
 
         # Read the data from the database and enter it into the relevant column
         title = datalayer.StockTitle(db)
@@ -121,9 +180,11 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         self.refreshColumn(author, 1, table, 150)
         # Set numeric to True for price and quantity
         price = datalayer.StockListPrice(db)
-        self.refreshColumn(price, 2, table, 50, True)
+        self.refreshColumn(price, 2, table, 50, True, True)
         quantity = datalayer.StockQuantity(db)
         self.refreshColumn(quantity, 3, table, 50, True)
+
+        header.setSortIndicatorShown(False)
 
     def addStock(self):
 
@@ -131,28 +192,6 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         stock_dialog = stockdialog.StockDialog()
         stock_dialog.exec_()
         self.refreshStock()
-
-    def stockHeaderSectionClicked(self, logicalindex: int):
-        # Find the stock table widget and header row
-        table: QTableWidget = self.findStockTable()
-        header: QHeaderView = table.horizontalHeader()
-
-        # Check if the table is currently sorted in ascending order and if so sort in descending order
-        if self.selectedStockHeader != logicalindex:
-            table.sortItems(logicalindex, QtCore.Qt.AscendingOrder)
-            # Change indicator on the header
-            header.setSortIndicator(logicalindex, QtCore.Qt.AscendingOrder)
-            # set selectedStockHeader to the new selected header index
-            self.selectedStockHeader = logicalindex
-        else:
-            table.sortItems(logicalindex, QtCore.Qt.DescendingOrder)
-            # Change indicator on the header
-            header.setSortIndicator(logicalindex, QtCore.Qt.DescendingOrder)
-            # reset selectedStockHeader as the column is currently sorted in descending order
-            self.selectedStockHeader = None
-
-        # Show the indicator
-        header.setSortIndicatorShown(True)
 
     def searchStock(self):
         pass
@@ -168,6 +207,7 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
     def refreshCustomers(self):
         # Find the customer table widget
         table: QTableWidget = self.findCustomerTable()
+        header: QHeaderView = table.horizontalHeader()
 
         # Read the data from the database and enter it into the relevant column
         names = datalayer.CustomerName(db)
@@ -182,7 +222,9 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         discount = datalayer.CustomerDiscount(db)
         self.refreshColumn(discount, 4, table, 50, True)
         totalSpent = datalayer.CustomerTotalSpent(db)
-        self.refreshColumn(totalSpent, 5, table, 50, True)
+        self.refreshColumn(totalSpent, 5, table, 50, True, True)
+
+        header.setSortIndicatorShown(False)
 
     def addCustomer(self):
 
@@ -190,28 +232,6 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         customer_dialog = customerdialog.CustomerDialog()
         customer_dialog.exec_()
         self.refreshCustomers()
-
-    def customerHeaderSectionClicked(self, logicalindex: int):
-        # Find the customer table widget and header row
-        table: QTableWidget = self.findCustomerTable()
-        header: QHeaderView = table.horizontalHeader()
-
-        # Check if the table is currently sorted in ascending order and if so sort in descending order
-        if self.selectedCustomerHeader != logicalindex:
-            table.sortItems(logicalindex, QtCore.Qt.AscendingOrder)
-            # Change indicator on the header
-            header.setSortIndicator(logicalindex, QtCore.Qt.AscendingOrder)
-            # Set selectedCustomerHeader to the new selected header index
-            self.selectedCustomerHeader = logicalindex
-        else:
-            table.sortItems(logicalindex, QtCore.Qt.DescendingOrder)
-            # Change an indicator on the header
-            header.setSortIndicator(logicalindex, QtCore.Qt.DescendingOrder)
-            # Reset selectedStockHeader as the column is currently sorted in descending order
-            self.selectedCustomerHeader = None
-
-        # Show the indicator
-        header.setSortIndicatorShown(True)
 
     def searchCustomers(self):
         pass
@@ -226,6 +246,7 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
     def refreshHeader(self):
         # Find the stock table widget
         table: QTableWidget = self.findHeadersTable()
+        header: QHeaderView = table.horizontalHeader()
 
         # Read the data from the database and enter it into the relevant column
         customerName = datalayer.OrderCustomerName(db)
@@ -234,7 +255,7 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         self.refreshColumn(deliveryAddress, 1, table, 100)
         # Set numeric True for delivery charge
         deliveryCharge = datalayer.OrderDeliveryCharge(db)
-        self.refreshColumn(deliveryCharge, 2, table, 75, True)
+        self.refreshColumn(deliveryCharge, 2, table, 75, True, True)
         date = datalayer.OrderDate(db)
         self.refreshColumn(date, 3, table, 75)
         totalCost = datalayer.OrderCost(db)
@@ -243,6 +264,8 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         orderNumber = datalayer.OrderNumber(db)
         self.refreshColumn(orderNumber, 5, table, 0)
 
+        header.setSortIndicatorShown(False)
+
     def addHeader(self):
 
         # Open add header dialog and then refresh header
@@ -250,31 +273,12 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         header_dialog.exec_()
         self.refreshHeader()
 
-    def headerHorizontalHeaderSectionClicked(self, logicalindex: int):
-        # Find the stock table widget and header row
-        table: QTableWidget = self.findHeadersTable()
-        header: QHeaderView = table.horizontalHeader()
-
-        # Check if the table is currently sorted in ascending order and if so sort in descending order
-        if self.selectedHeaderHeader != logicalindex:
-            table.sortItems(logicalindex, QtCore.Qt.AscendingOrder)
-            # Change indicator on the header
-            header.setSortIndicator(logicalindex, QtCore.Qt.AscendingOrder)
-            # Set selectedCustomerHeader to the new selected header index
-            self.selectedHeaderHeader = logicalindex
-        else:
-            table.sortItems(logicalindex, QtCore.Qt.DescendingOrder)
-            # Change indicator on the header
-            header.setSortIndicator(logicalindex, QtCore.Qt.DescendingOrder)
-            # Reset selectedHeaderHeader as the column is currently sorted in descending order
-            self.selectedHeaderHeader = None
-        header.setSortIndicatorShown(True)
-
     def headerVerticalHeaderSectionClicked(self, logicalindex: int):
         headersTable: QTableWidget = self.findHeadersTable()
 
         # find Order Number that has been selected
         self.selectedOrderNumber = headersTable.item(logicalindex, 5).text()
+        self.selectedCustomerName = headersTable.item(logicalindex, 0).text()
 
         # refresh the OrderLines for relevant order number
         self.refreshOrderLines()
@@ -292,7 +296,8 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
         quantity = datalayer.OrderQuantity(db, self.selectedOrderNumber)
         self.refreshColumn(quantity, 1, linesTable, 100, True)
         lineCost = datalayer.OrderLineCost(db, self.selectedOrderNumber)
-        self.refreshColumn(lineCost, 2, linesTable, 150, True)
+        self.refreshColumn(lineCost, 2, linesTable, 110, True, True)
+
 
     def findLinesTable(self) -> QTableWidget:
         # Find the order headers table widget
@@ -303,7 +308,7 @@ class FirstWindow(QtWidgets.QMainWindow, win1):
 
     def addLines(self):
         if self.selectedOrderNumber is not None:
-            line_dialog = linedialog.LineDialog(self.selectedOrderNumber)
+            line_dialog = linedialog.LineDialog(self.selectedOrderNumber, self.selectedCustomerName)
             line_dialog.exec_()
             self.refreshOrderLines()
         else:
